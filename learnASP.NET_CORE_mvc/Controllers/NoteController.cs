3445824885e4af19c204e2157f9856e6472b6abf
Fima <kolyshkin.efim@mail.ru>
2022-12-05 20:20:21 +0300

    done

--- learnASP.NET_CORE_mvc/Controllers/NoteController.cs
+++ learnASP.NET_CORE_mvc/Controllers/NoteController.cs
@@ -1,6 +1,7 @@
 ﻿using learnASP.NET_CORE_mvc.Data;
 using learnASP.NET_CORE_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace learnASP.NET_CORE_mvc.Controllers
 {
@@ -10,13 +11,13 @@ namespace learnASP.NET_CORE_mvc.Controllers
 
         public NoteController(dbContext context)
         {
-            _context= context;
+            _context = context;
         }
-        
+
         public IActionResult Index()
         {
             IEnumerable<Note> notesList = _context.Notes;
-            return View();
+            return View(notesList);
         }
         //get
         public IActionResult Create()
@@ -25,9 +26,62 @@ namespace learnASP.NET_CORE_mvc.Controllers
         }
         //post
         [HttpPost]
-        public IActionResult Create(Note object)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Note obj)
         {
-            return View();
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("inputError", "name input and description input are the same");
+            }
+            if (ModelState.IsValid)
+            { 
+            _context.Notes.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var editedNote = _context.Notes.Find(id);
+            if (editedNote == null)
+            {
+                return NotFound();
+            }
+            return View(editedNote);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Note obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("updateError", "updated inputs are the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Notes.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var deletedNote = _context.Notes.Find(id);
+            if (deletedNote!= null)
+            {
+                _context.Notes.Remove(deletedNote);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("index");   
         }
     }
 }
--- learnASP.NET_CORE_mvc/Migrations/20221205135209_AddNotesToDB.Designer.cs
+++ learnASP.NET_CORE_mvc/Migrations/20221205135209_AddNotesToDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using learnASP.NET_CORE_mvc.Data;
 
@@ -11,9 +12,11 @@ using learnASP.NET_CORE_mvc.Data;
 namespace learnASP.NETCOREmvc.Migrations
 {
     [DbContext(typeof(dbContext))]
-    partial class dbContextModelSnapshot : ModelSnapshot
+    [Migration("20221205135209_AddNotesToDB")]
+    partial class AddNotesToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
